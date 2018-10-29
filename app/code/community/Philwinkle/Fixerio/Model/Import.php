e59f0253fd08fce0c9108f9daf23754ed92211bf
Oussama Chebbi <oussama.chebbi@the-oz.com>
2018-10-29 15:43:02 +0100

    Limit api calls to base currency

--- app/code/community/Philwinkle/Fixerio/Model/Import.php
+++ app/code/community/Philwinkle/Fixerio/Model/Import.php
@@ -79,11 +79,23 @@ class Philwinkle_Fixerio_Model_Import extends Mage_Directory_Model_Currency_Impo
         try {
             $url = Mage::helper('core/url')->addRequestParam($this->getEndpointUrl(), $queryParams);
 
-            $response = $this->_httpClient
+            $ch = curl_init();
+
+            // set URL and other appropriate options
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, Mage::getStoreConfig('currency/fixerio/timeout'));
+
+            // grab URL and pass it to the browser
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            /*$response = $this->_httpClient
                 ->setUri($url)
                 ->setConfig(array('timeout' => Mage::getStoreConfig('currency/fixerio/timeout')))
                 ->request('GET')
-                ->getBody();
+                ->getBody();*/
 
             /** Second parameter is objectDecodeType - Zend_Json::TYPE_ARRAY, or Zend_Json::TYPE_OBJECT */
             $converted = Mage::helper('core')->jsonDecode($response, Zend_Json::TYPE_ARRAY);
@@ -113,6 +125,111 @@ class Philwinkle_Fixerio_Model_Import extends Mage_Directory_Model_Currency_Impo
             Mage::logException($e);
             if ($retry === 0) {
                 return $this->_convert($currencyFrom, $currencyTo, 1);
+            }
+
+            $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s.', $url);
+        }
+
+        return null;
+    }
+
+
+    public function fetchRates()
+    {
+        $fetchOnlyBase = Mage::getStoreConfig('currency/fixerio/fetch_only_base');
+        if(!$fetchOnlyBase) return parent::fetchRates();
+        $data = array();
+        $currencies = $this->_getCurrencyCodes();
+        @set_time_limit(0);
+        $baseCurrency = Mage::getStoreConfig('currency/fixerio/base');
+        $defaultCurrencies = $this->_getDefaultCurrencyCodes();
+        foreach ($defaultCurrencies as $currencyFrom) {
+            if($currencyFrom != $baseCurrency) continue;
+            if (!isset($data[$currencyFrom])) {
+                $data[$currencyFrom] = array();
+            }
+            $convertedRates = $this->_convertAll($currencyFrom, $currencies);
+            foreach ($convertedRates as $currencyTo => $currencyValue) {
+                if ($currencyFrom == $currencyTo) {
+                    $data[$currencyFrom][$currencyTo] = $this->_numberFormat(1);
+                } else {
+                    $data[$currencyFrom][$currencyTo] = $this->_numberFormat($currencyValue);
+                    $data[$currencyTo][$currencyFrom] = $this->_numberFormat(1 / $currencyValue);
+                }
+            }
+            ksort($data[$currencyFrom]);
+        }
+
+        return $data;
+    }
+
+    /**
+     * _convert
+     *
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param int    $retry
+     *
+     * @return float|null
+     */
+    protected function _convertAll($currencyFrom, $currencyTo = array(), $retry = 0)
+    {
+
+        $queryParams = array(
+            'access_key' => $this->_getConfigAccessKey(),
+            'BASE'       => $currencyFrom,
+            'symbols'    => implode(',', $currencyTo)
+        );
+
+        if (!$queryParams['access_key']) {
+            return null;
+        }
+
+        try {
+            $url = Mage::helper('core/url')->addRequestParam($this->getEndpointUrl(), $queryParams);
+            $result = array();
+            $ch = curl_init();
+
+            // set URL and other appropriate options
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, Mage::getStoreConfig('currency/fixerio/timeout'));
+
+            // grab URL and pass it to the browser
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            /** Second parameter is objectDecodeType - Zend_Json::TYPE_ARRAY, or Zend_Json::TYPE_OBJECT */
+            $converted = Mage::helper('core')->jsonDecode($response, Zend_Json::TYPE_ARRAY);
+
+            if (isset($converted['success'])) {
+                if (!$converted['success']) {
+                    $this->_messages[] = Mage::helper('directory')->__('Api Error: %s', $converted['error']['info']);
+                    Mage::throwException($converted['error']['info']);
+                }
+
+                if (isset($converted['rates']) && $rates = $converted['rates']) {
+                    foreach ($currencyTo as $currency){
+                        if (isset($rates[$currency])) {
+                            // test for bcmath to retain precision
+                            if (function_exists('bcadd')) {
+                                $result[$currency] = bcadd($rates[$currency], '0', 12);
+                            }else{
+                                $result[$currency] = (float) $rates[$currency];
+                            }
+
+                        }
+                    }
+                    return $result;
+                }
+
+                Mage::throwException('Error fetching currency rates from API response');
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
+            if ($retry === 0) {
+                return $this->_convertAll($currencyFrom, $currencyTo, 1);
             }
 
             $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s.', $url);
